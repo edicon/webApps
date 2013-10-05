0027fa332639dce715b5cf2fd7177840bf475502
edicon <hslee.edicon@gmail.com>
2013-10-05 16:51:03 +0900

    WebApp and HomeScreen

--- src/com/edicon/lib/webapp/WebAppActivity.java
+++ src/com/edicon/lib/webapp/WebAppActivity.java
@@ -1,15 +1,45 @@
 package com.edicon.lib.webapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TextView;
 
+/**
+ * @date	2013.1010
+ * 			Android Chrome Version 25부터, 그이전버전은 <iframe src="paulsawesomeapp://page1"> </iframe>
+ * 			설치되어 있지 않거나 scheme://host/가 Match되지 않으면 패키지 이름으로 Player Store로 이동함.
+ * @link	https://developers.google.com/chrome/mobile/docs/intents?hl=ko
+ * 
+ * @author 	EDICON. HyunSoo Lee
+ *
+ */
 public class WebAppActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_web_app);
+		
+		Intent i = getIntent();
+		
+		String action 	= i.getAction();
+		String scheme 	= i.getScheme();
+		String data		= i.getDataString(); 
+		String type		= i.getType();
+		String pkg		= i.getPackage();
+		Uri uriData 	= i.getData();
+		String webIntent	
+					= "Action: " 	+ action + "\n"
+					+ " scheme: "	+ scheme + "\n"
+					+ " data: " 	+ data + "\n"
+					+ " type: " 	+ type + "\n"
+					+ " pkg: " 		+ pkg + "\n"
+					+ " uriData: " 	+ uriData;
+		TextView webText = (TextView) findViewById(R.id.webapp_text);
+		webText.setText(webIntent);
 	}
 
 	@Override
